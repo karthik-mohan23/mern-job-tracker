e1c46a35af2d25e431ec9f241d6264ca52755340
karthik-mohan23 <karthikmohan2310@gmail.com>
2024-06-02 14:33:21 +0530

    built error page

--- client/src/pages/Error.jsx
+++ client/src/pages/Error.jsx
@@ -1,4 +1,28 @@
+import { useRouteError } from "react-router-dom";
+
 function Error() {
-  return <div>Error</div>;
+  const error = useRouteError();
+
+  if (error.status === 404) {
+    return (
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content text-center">
+          <div className="max-w-md">
+            <h1 className="text-5xl font-bold">{error.status}</h1>
+            <p className="py-6">Oops! Page Not Found.</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <h1 className="text-5xl font-bold">{error.statusText}</h1>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default Error;
