f4a154de8b72393c833a773aec5f711df51a7d8f
karthik-mohan23 <karthikmohan2310@gmail.com>
2024-06-03 09:57:46 +0530

    setup dashboard structure

--- client/src/pages/DashboardLayout.jsx
+++ client/src/pages/DashboardLayout.jsx
@@ -1,4 +1,39 @@
+import { Outlet } from "react-router-dom";
+import { TfiAngleDoubleRight } from "react-icons/tfi";
+
 function DashboardLayout() {
-  return <div>DashboardLayout</div>;
+  return (
+    <div>
+      <section className="max-w-7xl mx-auto">
+        <div className="drawer lg:drawer-open">
+          <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+          <div className="drawer-content">
+            {/* Page content here */}
+            <Outlet />
+            <label
+              htmlFor="my-drawer-2"
+              className="btn  drawer-button lg:hidden">
+              <TfiAngleDoubleRight />
+            </label>
+          </div>
+          <div className="drawer-side">
+            <label
+              htmlFor="my-drawer-2"
+              aria-label="close sidebar"
+              className="drawer-overlay"></label>
+            <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+              {/* Sidebar content here */}
+              <li>
+                <a>Sidebar Item 1</a>
+              </li>
+              <li>
+                <a>Sidebar Item 2</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
 export default DashboardLayout;
