34a013f2f3ec7de678e2144bfb3f57b5d962385c
karthik-mohan23 <karthikmohan2310@gmail.com>
2024-06-04 09:46:44 +0530

    added middleware to handle page not found error

--- server.js
+++ server.js
@@ -11,6 +11,11 @@ app.get("/", function (req, res) {
   res.send("Hello World");
 });
 
+// Not Found Middleware
+app.use("*", (req, res) => {
+  res.status(404).json({ msg: "not found" });
+});
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log("Server listening on port " + port));
