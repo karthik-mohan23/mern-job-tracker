15cf8f21cb182092ba5678c9e3f96b59eb56344f
karthik-mohan23 <karthikmohan2310@gmail.com>
2024-06-06 09:57:14 +0530

    hash password

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 import User from "../models/userModel.js";
 import { errorHandler } from "../utils/errorHandler.js";
+import { hashPassword } from "../utils/passwordUtils.js";
 import { UserSchema } from "../validationSchema/userSchema.js";
 
 const registerUser = async (req, res, next) => {
@@ -15,12 +16,16 @@ const registerUser = async (req, res, next) => {
       return next(errorHandler(400, "user already exists"));
     }
 
+    // hash password
+    const hashedPassword = await hashPassword(req.body.password);
+    req.body.password = hashedPassword;
+
     // to add an admin user
     const isFirstAccount = (await User.countDocuments()) === 0;
     req.body.role = isFirstAccount ? "admin" : "user";
 
-    const newUser = await User.create(req.body);
-    res.status(201).json(newUser);
+    await User.create(req.body);
+    res.status(201).json({ msg: "User successfully registered" });
   } catch (error) {
     console.log(error);
     next(error);
